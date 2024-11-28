13ab94aecbe06666fea8ceb93371ebbf2658c3d7
martinsfelipeg <martinsfelipeg@gmail.com>
2024-11-27 22:30:50 -0300

    added app and routes

--- brain_agriculture/app.py
+++ brain_agriculture/app.py
@@ -1,8 +1,15 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
+from brain_agriculture.routers import users, auth
+from brain_agriculture.schemas import Message
+
+
 app = FastAPI()
+app.include_router(users.router)
+app.include_router(auth.router)
 
-
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
