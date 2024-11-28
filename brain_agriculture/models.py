9bcc314e3d4d97951ecf9a3506bd6920d238c484
martinsfelipeg <martinsfelipeg@gmail.com>
2024-11-28 00:02:39 -0300

    table for farmers

--- brain_agriculture/models.py
+++ brain_agriculture/models.py
@@ -14,6 +14,18 @@ class User:
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
-    created_at: Mapped[datetime] = mapped_column(
-        init=False, server_default=func.now()
-    )
+
+@table_registry.mapped_as_dataclass
+class Farmer:
+    __tablename__ = 'farmers'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    ndoc: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str]
+    farm_name: Mapped[str]
+    city: Mapped[str]
+    state: Mapped[str]
+    total_area: Mapped[float]
+    arable_area: Mapped[float]
+    vegetation_area: Mapped[float]
+    planted_crops: Mapped[str]
